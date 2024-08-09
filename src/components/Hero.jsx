49f17b9ec85c1fba654f2ef384ee68356a68758d
mattiamarcon <mattiamarcon05@gmail.com>
2024-08-09 10:15:07 +0200

    parallax text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,10 +10,10 @@ function Hero() {
 
   return (
     <section className='my-2 w-full relative flex items-center justify-center'>
-        <img src="bancoGelati.jpg" alt="" className='saturate-0 w-full min-h-[580px] max-h-[650px] object-cover mx-auto ' />
+        <img src="bancoGelati.jpg" alt="" className='saturate-50 w-full min-h-[580px] max-h-[650px] object-cover mx-auto ' />
         <div id='heroElements' className='absolute flex flex-col items-center text-center gap-10 opacity-0 translate-y-20 p-5 '>
             <p className='text-white text-5xl lg:text-6xl'>Bontà e tradizione dal 1989</p>
-            <a href='#ciccio' className='w-fit p-5 bg-blue-400 hover:bg-blue-500 rounded-md border-black text-white text-3xl border-2'>SCOPRI DI PIU'</a>
+            <a href='#parallax' className='w-fit p-5 bg-blue-400 hover:bg-blue-500 rounded-md border-black text-white text-3xl border-2'>SCOPRI DI PIU'</a>
         </div>
         
     </section>
--- src/components/Parallalax.jsx
+++ src/components/Parallalax.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger.js";
+import {useGSAP} from "@gsap/react"
 
 function Parallalax() {
+
+  useGSAP(()=>{
+    gsap.registerPlugin(ScrollTrigger);
+
+    // gsap.to("#parallaxText",{
+    //   scrollTrigger:{
+    //     trigger:"#parallaxText",
+    //   },
+    //   opacity:1,
+    //   y:0,
+    //   delay:1,
+    // })
+
+    gsap.to(".pElements",{
+      scrollTrigger:{
+        trigger: ".pElements",
+      },
+      y:0,
+      stagger:1,
+      delay:0.5,
+      duration:1,
+      opacity:1,
+      ease:"back"
+    })
+  },[]);
+
   return (
-    <div className='h-lvh bg-green-50 flex items-center '>
-        <div id='ciccio' className='h-1/2 bg-yellow-400 w-full'>Parallalax</div>
-    </div>
+    <section id='parallax' className='flex max-lg:flex-col'>
+      <div className='w-full h-96'>
+
+      </div>
+      <video muted autoPlay loop={true} className='w-36 lg:w-60 mx-auto'>
+        <source src='animation.mp4' type='video/mp4'  />
+      </video>
+      <div id='parallaxText' className='opacity-1 flex flex-col items-center justify-center w-full text-4xl'>
+        <p className='pElements p1'>GUSTOSO</p>
+        <p className='pElements p2'>RINFRESCANTE</p>
+        <p className='pElements p3'>DOLCEZZA</p>
+        <p></p>
+      </div>
+    </section>
     
   )
 }
