72c43eeb266ad58dfb7e52936b4df694c2e0b648
mattiamarcon <mattiamarcon05@gmail.com>
2024-08-17 11:29:24 +0200

    afa

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,8 +20,8 @@ function Hero() {
     <section className='my-2 w-full relative flex items-center justify-center'>
         <img src="banco.jpg" alt="" className='saturate-50 opacity-85 w-full h-lvh  object-cover mx-auto ' />
         <div id='heroElements' className='absolute flex flex-col items-center text-center gap-10 opacity-0 translate-y-20 p-5 '>
-            <p className='text-white text-7xl lg:text-9xl tracking-wider'>Bontà e tradizione dal 1989</p>
-            <a id='bFindMore' href='#parallax' className='w-fit p-5 bg-blue-400 hover:bg-blue-500 rounded-md border-black text-white text-5xl border-2 translate-y-10'>SCOPRI DI PIU'</a>
+            <p className='text-white text-5xl lg:text-8xl tracking-wider'>Bontà e tradizione dal 1989</p>
+            <a id='bFindMore' href='#parallax' className='w-fit p-5 bg-blue-400 hover:bg-blue-500 rounded-md border-black text-white text-4xl border-2 translate-y-10'>SCOPRI DI PIU'</a>
         </div> 
     </section>
   )
